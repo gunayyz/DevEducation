c50796f1d82441c0b3ef11078695f1dcf39bd245
gunayyz <100687596+gunayyz@users.noreply.github.com>
2022-11-15 17:57:43 +0400

    Add files via upload

---
+++ JS TASKS/task2/js/script.js
@@ -0,0 +1,21 @@
+//1. The user enters 1 number (A). Print all numbers from 1 to 1000 that are divisible by A.
+
+
+
+
+
+
+//2. The user enters 1 number (A). Find the number of positive integers whose square is less than A (n^2 < A).
+
+
+
+
+//3. The user enters 1 number (A). Print the largest divisor (other than A itself) of the number A.
+
+
+
+
+//4. The user enters 2 numbers (A and B). Print the sum of all numbers from the range A to B that are divisible by 7 without a remainder. (Note that B may be less than A if you type).
+//5. The user enters 1 positive number (N). Print the Nth number of the fibonacci series. In the fibonacci series, each next number is the sum of the previous two. The first and second are considered equal to
+//6. The user enters 2 numbers. Find their greatest common divisor using Euclid's algorithm. 
+//7. The user enters 1 number. Find a number that is a mirror image of the sequence of digits of a given number, for example, given the number 123, output is 321 in this case
