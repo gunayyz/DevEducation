bb94dd5eeda76d4c2fb0525470437a0785df6a7f
gunayyz <100687596+gunayyz@users.noreply.github.com>
2022-11-15 17:41:32 +0400

    Add files via upload

---
+++ JS TASKS/task1/main.js
@@ -0,0 +1,60 @@
+
+//TASK1
+//1. User gives 2 numbers (A and B). Print to the console the result of dividing A by B and the remainder of the division.
+
+// let a=prompt("enter a");
+// let b=prompt("enter b");
+// let remain=a%b;
+// let result=a/b;
+// console.log(result,remain);
+
+//2. The user enters 2 values (A and B). Swap the contents of variables A and B (that means the value of variable B must be assigned to variable A, and vice versa).
+
+// let a=prompt("enter a"); 
+// let b=prompt("enter b"); 
+// let c=a; 
+// a=b; 
+// b=c;
+// console.log(a,b);
+
+//3. The user enters 3 non-zero numbers (A, B and C). Print to the console the solution (the value of X) of the standard linear equation, where A*X+B=C.
+
+// let a=prompt("enter a");
+// let b=prompt("enter b");
+// let c=prompt("enter c"); 
+// let x=c-b/a;
+// console.log(x);
+
+//4. User gives 2 numbers (A and B). If A>B then print to the console the result of A+B, if A=B then the result of A*B, if A<B then A-B.
+
+// let a=prompt("enter a"); 
+// let b=prompt("enter b"); 
+// if(a>b){
+//     console.log(a+b);
+// }
+// else if(a<b){
+//     console.log(a-b);
+// }
+// else if(a=b){
+//     console.log(a*b);
+// }
+
+//5. User gives 3 numbers (a, b, c). Find out and print to the console the solution of quadratic equation ax^2+bx+c=0.
+// let a=prompt("enter a");
+// let b=prompt("enter b");
+// let c=prompt("enter c"); 
+// let d=b*b-4*a*c;
+// if(d>0){
+//     root1 = (-b + Math.sqrt(d)) / (2 * a);
+//     root2 = (-b - Math.sqrt(d)) / (2 * a);
+//     console.log("First root is", root1)
+//     console.log("second root is", root2)
+// }
+// else if (d == 0) {
+//     root1 = root2 = -b / (2 * a);
+//     console.log("The root is", root1)
+// }
+// // if roots are not real
+// else {
+//     console.log("no real roots")
+// }
