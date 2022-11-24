eacdf917ec9659333a5e68355048826d848e1199
gunayyz <100687596+gunayyz@users.noreply.github.com>
2022-11-24 13:29:25 +0400

    Update script.js

--- JS TASKS/task2/js/script.js
+++ JS TASKS/task2/js/script.js
@@ -2,7 +2,7 @@
 
 // let a=+prompt("enter a");
 // while (isNaN(a) || a==""){
-//     a=+prompt("Please try again. Value must be a number:");
+//     a=+prompt("Please try again. you should enter a number:");
 //    }
 // for (let i = 0; i < 1000; i++) {
 //     if (i%a==0) {
@@ -17,7 +17,7 @@
 
 // let a=+prompt("enter a");
 // while (isNaN(a) || a==""){
-//     a=+prompt("Please try again. Value must be a number:");
+//     a=+prompt("Please try again. you should enter a number:");
 //    }
 // for (let i = 0; i < a; i++) {
 //     if (i*i<a) {
@@ -30,7 +30,7 @@
 
 // let a=+prompt("enter a");
 // while (isNaN(a) || a==""){
-//     a=+prompt("Please try again. Value must be a number:");
+//     a=+prompt("Please try again. you should enter a number:");
 //    }
 // let largestDiv=0;
 // for (let div = 1; div < a; div++) {
@@ -42,8 +42,32 @@
 
 
 
+//4. The user enters 2 numbers (A and B). Print the sum of all numbers from the range
+// A to B that are divisible by 7 without a remainder. (Note that B may be less than A if you type).
+//12 and 20 
 
-//4. The user enters 2 numbers (A and B). Print the sum of all numbers from the range A to B that are divisible by 7 without a remainder. (Note that B may be less than A if you type).
+let a=+prompt("enter a");
+
+while (isNaN(a) || a==""){
+    a=+prompt("Please try again.you should enter a number:");
+   }
+let b=+prompt("enter b");
+while (isNaN(b) || b==""){
+    b=+prompt("Please try again.you should enter a number:");
+   }
+let sum=0;
+while (a>b) {
+    
+}
+for (let i = 1; i < a; div++) {
+    if (condition) {
+        
+    }
+}
+
+
+
+
 //5. The user enters 1 positive number (N). Print the Nth number of the fibonacci series. In the fibonacci series, each next number is the sum of the previous two. The first and second are considered equal to
 //6. The user enters 2 numbers. Find their greatest common divisor using Euclid's algorithm. 
 //7. The user enters 1 number. Find a number that is a mirror image of the sequence of digits of a given number, for example, given the number 123, output is 321 in this case
